09bf9a842a8bafc483dea3b7b7348130970cc5f0
ethancoote <realethancoote@gmail.com>
2026-01-01 22:23:13 -0500

    updating portfolio pics

--- src/scripts/lightbox.js
+++ src/scripts/lightbox.js
@@ -1,15 +1,18 @@
-const lightboxClose = document.querySelector("#lightboxClose");
 const lightbox = document.querySelector(".lightbox");
 const portfolioItems = document.querySelectorAll(".portfolioItem");
 const lightboxImg = document.querySelector("#lightboxImg");
 
-const homepages = ["images/portfolio/chalkieFull.webp", "images/portfolio/bimFull.webp", "images/portfolio/apexFull.webp", "images/portfolio/elevateFull.webp", "images/portfolio/arloFull.webp"];
+const homepages = ["images/portfolio/trueautoFull.webp",
+                    "images/portfolio/chalkieFull.webp", 
+                    "images/portfolio/nycurlsFull.webp", 
+                    "images/portfolio/bimFull.webp", 
+                    "images/portfolio/kansasvcFull.webp", 
+                    "images/portfolio/apexFull.webp",
+                    "images/portfolio/greenwayFull.webp", 
+                    "images/portfolio/elevateFull.webp", 
+                    "images/portfolio/arloFull.webp"];
 
 let open = false;
-lightboxClose.addEventListener("click", function() {
-    lightbox.style.display = "none";
-    open = false;
-});
 
 let i = 0;
 portfolioItems.forEach((item) => { 
